3d5999b30f1f114897b38d9bb5542bca9cfc78bf
NeroBirdy <nirvanav.39@gmail.com>
2026-04-21 15:53:21 +0500

    2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,6 @@
 import svgLoader from "vite-svg-loader";
 
 export default defineNuxtConfig({
-  app: {
-    baseURL: "/smartadmin-vk-miniapp/", // замените на имя вашего репозитория!
-    buildAssetsDir: "assets/", // меняем _nuxt на assets, чтобы избежать проблем с Jekyll
-  },
-  nitro: {
-    output: {
-      dir: ".output",
-      publicDir: ".output/public",
-    },
-  },
   css: ["~/assets/css/main.css"],
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
