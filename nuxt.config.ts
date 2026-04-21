d442d4e4783a7f51cabbf5c68c21204eec88a45d
NeroBirdy <131138425+NeroBirdy@users.noreply.github.com>
2026-04-21 09:44:42 +0500

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   app: {
     baseURL: '/smartadmin-vk-miniapp/', // замените на имя вашего репозитория!
-    buildAssetsDir: 'assets', // меняем _nuxt на assets, чтобы избежать проблем с Jekyll
+    buildAssetsDir: 'assets/', // меняем _nuxt на assets, чтобы избежать проблем с Jekyll
   },
 
   ssr: false, // важно для статического сайта
