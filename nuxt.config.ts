ac1fd27a5574880dbd98ed5268135810b37e6c4c
NeroBirdy <nirvanav.39@gmail.com>
2026-04-21 09:36:48 +0500

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  vite: {
-    server: {
-      allowedHosts: ['.ngrok-free.dev','.loca.lt', 'loca.lt', 'mean-radios-invite.loca.lt']
-    },
+  app: {
+    baseURL: '/smartadmin-vk-miniapp/', // замените на имя вашего репозитория!
+    buildAssetsDir: 'assets', // меняем _nuxt на assets, чтобы избежать проблем с Jekyll
   },
-  ssr: false,
+
+  ssr: false, // важно для статического сайта
+  
+  devtools: { enabled: true },
+  
+  // Убедитесь, что используется правильная директория для генерации
+  nitro: {
+    output: {
+      dir: '.output',
+      publicDir: '.output/public'
+    }
+  }
 })
