bc0a3dd349d094405d58e742811372a7aa2f03c5
NeroBirdy <131138425+NeroBirdy@users.noreply.github.com>
2026-06-22 16:07:48 +0500

    Update day.vue

--- app/pages/day.vue
+++ app/pages/day.vue
@@ -81,7 +81,7 @@ type Lesson = {
   };
 };
 
-const link = "https://90f80837-5e0d-4337-82d8-d2b67adb245c.tunnel4.com ";
+const link = "https://90f80837-5e0d-4337-82d8-d2b67adb245c.tunnel4.com";
 
 const isLoading = ref(true);
 const lessons = ref<Lesson[]>([]);
